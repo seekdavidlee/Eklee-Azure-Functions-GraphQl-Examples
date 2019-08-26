c6a9de9bd695425bccf33e38ccb07d360a29d4da
David Lee <seekdavidlee@gmail.com>
2019-08-25 20:19:13 -0500

    Initial checkin for usage of command arg.

--- Example.LoadTester/Example.LoadTester/Program.cs
+++ Example.LoadTester/Example.LoadTester/Program.cs
@@ -1,6 +1,8 @@
-﻿using Newtonsoft.Json;
+﻿using CommandLine;
+using Newtonsoft.Json;
 using System;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Example.LoadTester
@@ -9,19 +11,21 @@ namespace Example.LoadTester
 	{
 		static async Task Main(string[] args)
 		{
-			if (args.Length == 2)
+			await Parser.Default.ParseArguments<CommandLineArgs>(args)
+				.MapResult(async cmdArgs =>
 			{
 				Console.WriteLine("Running...");
-				var fileParser = new LoadTestFileParser(args[0]);
+
+				var fileParser = new LoadTestFileParser(cmdArgs.RunFilePath);
+
 				var loadTestRunner = new LoadTestRunner(await fileParser.GetLoadTestRunAsync());
-				var result = await loadTestRunner.RunAsync(args[1], Console.WriteLine);
-				var fileName = DateTime.Now.ToString("yyyyMMddHHmmss")+ ".txt";
+
+				var result = await loadTestRunner.RunAsync(cmdArgs.GraphQLUrl, Console.WriteLine);
+
+				var fileName = DateTime.Now.ToString("yyyyMMddHHmmss") + ".txt";
 				using (var sr = new StreamWriter(fileName))
 					sr.Write(JsonConvert.SerializeObject(result));
-
-				Console.WriteLine("Done. Press any key to continue.");
-				Console.ReadKey();
-			}
+			}, errors => Task.FromResult(0));
 		}
 	}
 }
