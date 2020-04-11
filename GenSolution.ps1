b0341a8e464aca09364a788cbfe943d42fb9b8a5
David Lee <seekdavidlee@gmail.com>
2020-04-11 16:13:06 -0500

    Upgrade GenSolution to gen v3 Azure Function

--- GenSolution.ps1
+++ GenSolution.ps1
@@ -19,7 +19,8 @@ $projectNupkgPath = "$wd\$release.nupkg"
 if ((Test-Path -Path $projectNupkgPath) -eq $false) {
     Write-Host "Downloading file $($result.projectTemplates)"
     Invoke-WebRequest -Uri $result.projectTemplates -OutFile $projectNupkgPath
-} else {
+}
+else {
     Write-Host "Found $projectNupkgPath"
 }
 
@@ -30,7 +31,8 @@ $templateNupkgPath = "$wd\$release-template.nupkg"
 if ((Test-Path -Path $templateNupkgPath) -eq $false) {
     Write-Host "Downloading file $($result.itemTemplates)"
     Invoke-WebRequest -Uri $result.itemTemplates -OutFile $templateNupkgPath
-} else {
+}
+else {
     Write-Host "Found $templateNupkgPath"
 }
 Write-Host "Installing $templateNupkgPath"
@@ -48,8 +50,8 @@ if ((Test-Path -Path $projectPath ) -eq $false) {
     Write-Host "ProjectPath: $projectPath"
     New-Item -ItemType Directory -Force -Path $projectPath
 
-	# Store current path.
-	$currentDir = (Get-Location).Path
+    # Store current path.
+    $currentDir = (Get-Location).Path
 
     Push-Location $projectPath
     dotnet new sln --name $projectName
@@ -66,7 +68,9 @@ if ((Test-Path -Path $projectPath ) -eq $false) {
     }
     
     dotnet add package Eklee.Azure.Functions.GraphQl -s $NugetSource
-	dotnet add package Microsoft.Extensions.Caching.Memory
+    dotnet add package Microsoft.NET.Sdk.Functions
+    dotnet add package Microsoft.Azure.WebJobs.Extensions.Http
+    dotnet add package Microsoft.Extensions.Caching.Memory
     
     Copy-Item $currentDir\Templates\* -Destination .\ -Recurse -Force
 
@@ -74,17 +78,18 @@ if ((Test-Path -Path $projectPath ) -eq $false) {
 
     $allFiles | ForEach-Object {        
         $path = $_.Directory.FullName + "\" + $_.Name
+        $refProjectName = $projectName.Replace("-", ".")
         $content = Get-Content -Path $path
-        $content = $content.Replace("namespace Example", "namespace $projectName")
-		$content = $content.Replace("using Example.", "using $projectName.")
+        $content = $content.Replace("namespace Example", "namespace $refProjectName")
+        $content = $content.Replace("using Example.", "using $refProjectName.")
         $content
-		Set-Content $content -Path $path
+        Set-Content $content -Path $path
     }
 
     $projectFullName = "$projectName.csproj"
     $projContent = Get-Content -Path .\$projectFullName
-	$projContent = $projContent.Replace("<TargetFramework>netcoreapp2.1</TargetFramework>","<TargetFramework>netstandard2.0</TargetFramework>")
-    $projContent = $projContent.Replace("<AzureFunctionsVersion></AzureFunctionsVersion>","<AzureFunctionsVersion>v2</AzureFunctionsVersion>")
+    $projContent = $projContent.Replace("<TargetFramework>netcoreapp2.1</TargetFramework>", "<TargetFramework>netstandard2.0</TargetFramework>")
+    $projContent = $projContent.Replace("<AzureFunctionsVersion>V2</AzureFunctionsVersion>", "<AzureFunctionsVersion>v3</AzureFunctionsVersion>")
     Set-Content $projContent -Path .\$projectFullName
 
     Pop-Location
